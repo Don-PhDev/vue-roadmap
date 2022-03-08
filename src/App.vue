3c71f8dc8ec234f1adc44d21560f7889e5480f45
Don Forrest <forrestusbal@gmail.com>
2022-03-08 16:35:18 +0800

    Conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,9 @@
 <template>
   <div>
-    <h3 :id="headingId">Heading</h3>
-    <button :disabled="isDisabled">Click me</button>
-    <h3 class="underline">Underlined text</h3>
-    <h3 class="underline" :class="status">Status</h3>
-    <h3 :class="isPromoted ? ['success', 'underline'] : 'danger' ">Promoted</h3>
-    <h3 :class="[isPromoted ? 'underline' : '', isSoldOut ? 'danger' : 'success' ]">Array conditional</h3>
-    <h3 :class="{
-         promoted: isPromoted,
-         new: !isSoldOut,
-         'sold-out': isSoldOut
-      }">Object conditional
-    </h3>
-    <h3 :style="{
-      color: highlight,
-      fontSize: headerSize + 'px'
-    }">Inline Style
-    </h3>
-    <h3 :style="headerStyles">Object style</h3>
-    <div :style="[baseStyle, successStyle]">Array style</div>
+    <p v-if="num === 0">The number is {{ num }}</p>
+    <p v-else-if="num < 0">Negative number: {{ num }}</p>
+    <p v-else>Positive number: {{ num }}</p>
+    <p v-show="show">Using v-show</p>
   </div>
 </template>
 
@@ -28,25 +13,8 @@ export default {
   name: 'App',
   data() {
     return {
-      headingId: "heading",
-      isDisabled: true,
-      status: "danger",
-      isPromoted: true,
-      isSoldOut: false,
-      highlight: "orange",
-      headerSize: 50,
-      headerStyles: {
-        color: "orange",
-        fontSize: "50px",
-      },
-      baseStyle: {
-        fontSize: "50px",
-        border: "1px solid yellow"
-      },
-      successStyle: {
-        color: "yellowgreen",
-        border: "1px solid black"
-      }
+      num: 0,
+      show: true
     }
   },
 }
@@ -60,23 +28,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.underline {
-  text-decoration: underline;
-}
-
-.danger {
-  color: red
-}
-
-.success {
-  color: green
-}
-
-.sold-out {
-  color: white;
-  background-color: blue;
-  padding: 1rem 1rem;
 }
 </style>
