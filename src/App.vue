d76a48d41aeed44e67b67ea3aadda24de2800d4c
Don Forrest <forrestusbal@gmail.com>
2022-03-08 15:41:56 +0800

    Binding HTML

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>{{ greet }} {{ name }}</h1>
-    <h3 v-text="language" />
+    <p v-html="language" />
   </div>
 </template>
 
@@ -13,7 +13,7 @@ export default {
     return {
       greet: "Hello",
       name: "Don",
-      language: "Vue JS",
+      language: "<b>Vue JS</b>",
     }
   },
 }
